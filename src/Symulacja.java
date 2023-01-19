3caa5ee223855de1afa5cd0a7380264874744cc4
lukaszbuelaw <lukas192012@gmail.com>
2023-01-19 23:51:03 +0100

    mobility

--- src/Symulacja.java
+++ src/Symulacja.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public abstract class Symulacja {
 
-    public static final double MOBILITY = 0.001;
+    public static final double MOBILITY = 0.007;
     public static final double ANIMAL_ILLNESS_PROPABILITY = 0.8;
     public static final double HUMAN_ILLNESS_PROBABLITY = 0.8;
     public static final double HEALTHY_ANIMAL_PROPABILITY = 0.99;
